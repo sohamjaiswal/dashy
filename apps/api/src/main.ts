4707e3e2ace0456d28b5fee694a58af315d637af
Soham Jaiswal <sohamjaiswal10@gmail.com>
2022-10-02 09:40:29 +0530

    feat: added dashy-bot

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { Message } from '@dashy/api-interfaces';
+import { BOT_TOKEN } from '../../../libs/secrets/src/lib/secrets';
 
 const app = express();
 
@@ -8,6 +9,10 @@ const greeting: Message = { message: 'Welcome to api!' };
 app.get('/api', (req, res) => {
   res.send(greeting);
 });
+
+app.get('/token', (_, res) => {
+  res.send(BOT_TOKEN)
+})
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
