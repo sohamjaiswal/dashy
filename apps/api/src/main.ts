14caf1c8f9ce6300d0a5cd4c11f0a58fafaa03fb
Soham Jaiswal <sohamjaiswal10@gmail.com>
2022-10-02 09:43:39 +0530

    fix: removed bot secret exposed route

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import { Message } from '@dashy/api-interfaces';
-import { BOT_TOKEN } from '../../../libs/secrets/src/lib/secrets';
 
 const app = express();
 
@@ -9,10 +8,6 @@ const greeting: Message = { message: 'Welcome to api!' };
 app.get('/api', (req, res) => {
   res.send(greeting);
 });
-
-app.get('/token', (_, res) => {
-  res.send(BOT_TOKEN)
-})
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
