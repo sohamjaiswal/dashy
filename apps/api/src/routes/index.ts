d78e45ee595480d54a21e8c70bcac4472ddb6eb8
Soham Jaiswal <sohamjaiswal10@gmail.com>
2022-10-18 06:52:07 +0530

    refactor: login, logout => session

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import {default as login} from "./login/login.routes"
-import {default as logout} from "./logout/logout.routes"
+import {default as session} from "./session/session.routes"
 import {default as register} from "./register/register.routes"
 
 const router = Router()
 
-router.use('/login', login)
-router.use('/logout', logout)
+router.use('/session', session)
 router.use('/register', register)
 
 export default router
